e68066a44831f0485e0437e22435924ab1a0f2af
luismoller <49380630+luismoller@users.noreply.github.com>
2020-10-01 21:38:16 -0700

    First GUI App in python

--- FirstGuiApp.py
+++ FirstGuiApp.py
@@ -12,7 +12,7 @@ class FirstGuiApp(object):
         master.title('Application')
 
         self.canvas = tk.Canvas(master, height=700, width=700, bg='#348feb')
-        self.canvas.pack()
+        self.canvas.grid()
 
         self.frame = tk.Frame(master, bg='white')
         self.frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=.1)
@@ -20,17 +20,17 @@ class FirstGuiApp(object):
         self.openAppButton = tk.Button(master, text="Open File", padx=10,
                                        pady=5, fg='white', bg='black',
                                        command=self.addApps)
-        self.openAppButton.pack()
+        self.openAppButton.grid(column=0, row=1)
 
         self.runAppsButton = tk.Button(master, text='Run Apps', padx=10,
                                        pady=5, fg='white', bg='black',
                                        command=self.runApps)
-        self.runAppsButton.pack()
+        self.runAppsButton.grid(column=1, row=1)
 
         self.closeAppsButton = tk.Button(master, text='Close Apps', padx=10,
                                          pady=5, fg='white', bg='black',
                                          command=self.closeApps)
-        self.closeAppsButton.pack()
+        self.closeAppsButton.grid(column=2, row=1)
 
         if os.path.isfile('save.txt'):
             self.apps = Apps.fromTextFile('save.txt')
@@ -64,6 +64,6 @@ class FirstGuiApp(object):
         msgBox = messagebox.askyesno('Exit Application',
                                      'Are you sure you want to close the applications?',
                                      icon='warning')
-        if msgBox == 'yes':
+        if msgBox is True:
             for app in self.apps.getApps():
                 app.closeApp()
